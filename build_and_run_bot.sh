74da333a93dec2224ae9c9bcb4adcaec57dae692
hglad <hans.glad@sesam.io>
2023-05-30 18:28:46 +0200

    Setting GIT_REVISION label

--- build_and_run_bot.sh
+++ build_and_run_bot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/hans/Projects/HansBot
 git pull
+
+export GIT_REVISION=$(git rev-parse HEAD)
 docker build -t hglad/hans-bot:latest .
 docker kill $(docker ps -q)
 docker run --restart unless-stopped --detach --env-file=env.list hglad/hans-bot:latest
