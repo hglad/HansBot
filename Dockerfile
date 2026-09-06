fa14f15e7cf07982cc8d5eac6543bbb293be9930
Hans Glad <hanseglad@gmail.com>
2026-09-06 20:33:12 +0200

    Update Python image to 3.10 and add Docker container name

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9-slim
+FROM python:3.10-slim
 MAINTAINER Hans Erlend Bakken Glad "hanseglad@gmail.com"
 
 RUN apt-get update
--- build_and_run_bot.sh
+++ build_and_run_bot.sh
@@ -4,4 +4,4 @@ git pull
 
 export GIT_REVISION=$(git rev-parse HEAD)
 docker build -t hglad/hans-bot:latest .
-docker run --restart unless-stopped --detach --env-file=env.list hglad/hans-bot:latest
+docker run --restart unless-stopped --detach --env-file=env.list --name "eggbot" hglad/hans-bot:latest
